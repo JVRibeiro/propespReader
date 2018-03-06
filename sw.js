100246441e796f7d50283a8c7ad08e705c18d69a
Victor Ribeiro <devjvribeiro@gmail.com>
2018-03-05 23:15:23 -0300

    *

--- sw.js
+++ sw.js
@@ -1,7 +1,10 @@
 var CACHE_NAME = 'scannerCache';
 var urlsToCache = [
   'external/instascan.js',
-  'external/jquery.min.js'
+  'external/jquery.min.js',
+  'https://fonts.googleapis.com/icon?family=Material+Icons',
+  'https://code.getmdl.io/1.3.0/material.indigo-red.min.css',
+  'https://code.getmdl.io/1.3.0/material.min.js'
 ];
 
 self.addEventListener('install', function(event) {
