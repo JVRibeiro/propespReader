4301dd3b218fac0420553bce291bf1d4425755d1
Victor Ribeiro <devjvribeiro@gmail.com>
2018-03-05 21:17:43 -0300

    *

--- sw.js
+++ sw.js
@@ -1,14 +1,15 @@
 var CACHE_NAME = 'scannerCache';
 var urlsToCache = [
-  '/',
-  '/external/instascan.js'
+  'index.html',
+  '/external/instascan.js',
+  '/external/jquery.min.js'
 ];
 
-self.addEventListener('install', function (event) {
+self.addEventListener('install', function(event) {
   // Perform install steps
   event.waitUntil(
     caches.open(CACHE_NAME)
-    .then(function (cache) {
+    .then(function(cache) {
       console.log('Opened cache');
       return cache.addAll(urlsToCache);
     })
