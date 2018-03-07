ab2e8bc6e4306999b1f6d3f9e6c0bae63006464d
Victor Ribeiro <devjvribeiro@gmail.com>
2018-03-07 03:47:23 -0300

    *

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
 let CACHE_NAME = 'scannerCache';
 let urlsToCache = [
   '/',
-  'js/scanner.js?v=0.0.34',
+  'js/scanner.js?v=0.0.35',
   'css/material-icons.css',
   'fonts/material-icons.woff2',
   'css/material.indigo-green.min.css',
