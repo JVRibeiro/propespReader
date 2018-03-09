34453c6368e641d6aba2811039683e0e15d01fd8
Victor Ribeiro <devjvribeiro@gmail.com>
2018-03-09 00:57:20 -0300

    *

--- js/scanner.js
+++ js/scanner.js
@@ -178,12 +178,12 @@ $('.mdl-layout__tab').on('click', function() {
     console.log('Camera deactivated.');
     isCameraTabActive = false;
     scanner.stop();
-  } else if (!this.href.match("#scroll-tab-1")) {
-    if (this.href.match("#scroll-tab-3")) {
-      setTimeout(function () {
-        document.querySelector('.mdl-layout__content').scrollTop = document.querySelector('.mdl-layout__content').scrollHeight;
-      },100);
-    }
+  }
+
+  if (this.href.match("#scroll-tab-3")) {
+    setTimeout(function () {
+      document.querySelector('.mdl-layout__content').scrollTop = document.querySelector('.mdl-layout__content').scrollHeight;
+    },100);
   }
 });
 //})();
